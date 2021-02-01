97e70c579dc60350fa6bfab7f44f2000a6ce574a
PatrickMurray78 <g00344530@gmit.ie>
2021-02-01 16:34:03 +0000

    Looked at the basics

---
+++ basics.py
@@ -0,0 +1,16 @@
+# Patrick Murray
+# Python basics
+# 2021-02-01
+
+print("Hello, world!")
+
+a = 1
+b = 1.0
+s = "Hello, world from a string!"
+t = 'Hello, from a different string'
+
+print(a, b, s, t)
+
+print(type(s))
+
+print(s[3:10:2])
