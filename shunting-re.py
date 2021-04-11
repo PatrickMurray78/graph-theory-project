36729eec100ec6a0a30e83ec63f55c7615a2af77
PatrickMurray78 <g00344530@gmit.ie>
2021-04-11 16:27:33 +0100

    Shunting yard algorithm for regex

--- shunting-re.py
+++ shunting-re.py
@@ -8,17 +8,17 @@ def shunt(infix):
     # The shunting yard operator stack.
     stack = ""
     # Operator precedence.
-    prec = {'*': 100,  '/': 90 ,'+': 80 , '-': 70}
+    prec = {'*': 100,  '.': 90 ,'|': 80}
     # Loop through the input a character at a time.
     for c in infix:
         # c is a digit.
-        if c in {'0','1','2', '3', '4', '5', '6', '7', '8', '9'}:
+        if c in {'a', 'b'}:
             # Push it to the output.
             postfix = postfix + c
         # c is an operator.
-        elif c in {'+', '-', '*', '/'}:
+        elif c in {'*', '.', '|'}:
             # Check what is on the stack.
-            while len(stack) > 0 and stack[-1] != '(' and prec[stack[-1]] > prec[c]:
+            while len(stack) > 0 and stack[-1] != '(' and prec[stack[-1]] >= prec[c]:
                 # Append operator at top of stack to output.
                 postfix = postfix + stack[-1]
                 # Remove operator from stack.
@@ -44,7 +44,8 @@ def shunt(infix):
     # Return the postfix
     return postfix
 
-for infix in ["3+4*(2-1)", "1+2+3+4+5*6", "(1+2)*(4*(6-7))"]:
-    print(f"infix: {infix}")
-    print(f"shunt: {shunt(infix)}")
-    print()
+if __name__ == "__main__":
+    for infix in ["a.(b.b)*.a"]:
+        print(f"infix: {infix}")
+        print(f"shunt: {shunt(infix)}")
+        print()
