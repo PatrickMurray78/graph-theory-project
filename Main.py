a1aa324ecd8cc30acf1788650204b5705b4b72f0
patrickmurray73 <g00344530@gmit.ie>
2021-05-01 12:48:14 +0100

    Commented all uncommented code

--- Main.py
+++ Main.py
@@ -11,38 +11,56 @@ parser.add_argument('--test', dest='command', action='store_const',
                     const='test',
                     help='run tests')
 
+# Let args equal the args parsed from the parser
 args = parser.parse_args()
 
+# If the command in args equals test, run the tests function in tests.py
 if args.command == 'test':
     tests.tests()
 
+# This function searches through a file and returns every line which matches the regular expression
 def searchFile(infix, filePath):
+    # Use count to keep track of which line we are on in file
     count = 1
     print(f"\nThe following lines match your regular expression {infix}")
     print("================================================================\n")
+    # Open the file located at filePath in read mode
     with open(filePath, 'r') as reader:
+        # For each line in the file
         for line in reader.readlines():
+            # Remove the newline character
             line = line.rstrip('\n')
+            # Get the postfix of the infix regex using shunt.py
             postfix = shunt.shunt(infix)
+            # Get the nfa of the postfix using thompson.py
             nfa = thompson.re_to_nfa(postfix)
+            # Match the nfa to the line of the file
             match = nfa.match(line)
+            # The line matches!
             if match == True:
+                # Output the line number and the line which match
                 print(f"Line {count}: {line}")
+            # Increment count
             count = count + 1
 
 # GUI for menu
 keepRunning = True
+# Keep running menu until keepRunning is set to false by selecting option 2 to exit
 while keepRunning:
     print("\n1. Search text file using regular expression")
     print("2. Exit")
     option = input("=> ")
 
+    # Search text file for lines matching the regular expression
     if option == "1":
         infix = input("\nEnter the regular expression: ")
         filePath = input("Enter the path to file: ")
+        # Call the searchFile function and pass the regular expression and file path as parameters
         searchFile(infix, filePath)
+    # Exit
     elif option == "2":
         print("Goodbye!")
+        # Set keepRunning to false which stops loop from running
         keepRunning = False
     else:
         print("Invalid option entered!")
--- tests.py
+++ tests.py
@@ -7,17 +7,24 @@ def tests():
             ["a.(b.b)*.a", ["aa", "abba", "aba"]],
             ["1.(0.0)*.1", ["11", "100001", "11001"]],
             ["a?.b?", ["a", "b", "ab", "abb"]],
-            ["a.b+.c+", ["abc", "abcc", "abbbbcc", "bc"]]
+            ["a.b+.c+", ["abc", "abcc", "abbbbcc", "bc"]],
+            ["c.o.l.o.u?.r", ["color", "colour", "colorr"]]
     ]
 
+    # For each test in tests
     for test in tests:
+        # Set infix to the first field in test
         infix = test[0]
         print(f"infix:   {infix}")
+        # Get the postfix of the infix regex using shunt.py
         postfix = shunt.shunt(infix)
         print(f"postfix: {postfix}")
+        # Get the nfa of the postfix using thompson.py
         nfa = thompson.re_to_nfa(postfix)
         print(f"thompson: {nfa}")
+        # For each string in the second field of test
         for s in test[1]:
+            # Match the nfa to the line of the file
             match = nfa.match(s)
             print(f"Match '{s}': {match}")
         print()
